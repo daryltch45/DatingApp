97797b209cab5583f2aa0c8ec3bdf600333b4e8f
Daryl  Tchienang <daryltchienang@Daryls-MacBook-Pro.local>
2024-12-06 00:19:48 +0100

    Feat: gitignore added.

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3b23f8b1321eebe1f1f019b86c4eee50ed1cdad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c7d0c7e63d44bfe6daf77a93f06cba38fb696c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
