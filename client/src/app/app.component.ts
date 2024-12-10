fb3dd7d84c1eae583d239b23273432de7ce56fe4
Daryl  Tchienang <daryltchienang@Daryls-MacBook-Pro.local>
2024-12-10 22:14:44 +0100

    Login, register Implemented.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,44 +1,32 @@
 import { NgFor } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor, NavComponent],
+  imports: [RouterOutlet, NgFor, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent implements OnInit{
-
-  http = inject(HttpClient);  // Dependency Injection
   title = 'DatingApp';
   users: any; 
   private accountService=inject(AccountService); 
 
   ngOnInit(): void {
-    this.getUsers(); 
+    this.setCurrentUser(); 
   }
-
-
 
   setCurrentUser(){
     const userString = localStorage.getItem('user'); 
     if(!userString) return; 
     const user= JSON.parse(userString); 
     this.accountService.currentUser.set(user); 
-  }
-
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: reponse => this.users = reponse, 
-      error: error => console.log(error), 
-      complete: () => console.log("Good Request !")
-    })
   }
 }
 
---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { RegisterComponent } from "../register/register.component";
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RegisterComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements OnInit {
+  registerMode=false; 
+  http=inject(HttpClient); 
+  users:any; 
+
+  ngOnInit(): void {
+    this.users = this.getUsers(); 
+  }
+
+  getUsers(){
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: reponse => this.users = reponse, 
+      error: error => console.log(error), 
+      complete: () => console.log("Good Request !")
+    })
+  }
+
+  registerToggle(){
+
+    this.registerMode=!this.registerMode
+  }
+}
